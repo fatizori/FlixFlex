2fce9049cc62637a8c79656795983d272712ae91
Fatima Zohra MEZERREG <fatizojs@gmail.com>
2023-01-22 09:32:28 +0100

    Updates

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/">
-    <img src="{{ asset('build/assets/img/FlixFlexLogo.png') }}" alt="FLIXFLEX Logo" class="w-50 h-20">
+    <img src="{{ asset('img/logo.png') }}" alt="FLIXFLEX Logo" class="w-50 h-20">
 
 </a>
